be17ac9260251786c189b04c9e60bff79e0ac7a8
MariQerdika <40671485+MariQerdika@users.noreply.github.com>
2022-04-10 16:48:04 +0400

    add main video play/pause button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,23 @@
+// burger menu
+let burger = document.querySelector(".burger");
+let nav = document.querySelector(".header__nav");
+let body = document.querySelector("body");
+burger.onclick = () => {
+    burger.classList.toggle("active");
+    nav.classList.toggle("active");
+    body.classList.toggle("scroll-none");
+}
 
-    let burger = document.querySelector(".burger");
-    let nav = document.querySelector(".header__nav");
-    let body = document.querySelector("body");
 
-    burger.onclick = () => {
-        burger.classList.toggle("active");
-        nav.classList.toggle("active");
-        body.classList.toggle("scroll-none");
-    }
+// main video play/pause
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".content__video");
+btn.addEventListener("click", function () {
+  if (!btn.classList.contains("slide")) {
+    btn.classList.add("slide");
+    video.pause();
+  } else {
+    btn.classList.remove("slide");
+    video.play();
+  }
+});
